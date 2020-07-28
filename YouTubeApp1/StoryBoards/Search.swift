e01aadba747e6f7ee4deda8beafba21193cd8b87
irietoshinari <60064033+irietoshinari@users.noreply.github.com>
2020-07-28 14:41:19 +0900

    Headerのアニメーションを作成をしたが、 headerのレイアウトが崩れてしまった。 今後はAutoLayoutを使って調整していこうと思う
    メイン画面がAPIの制限で映らなくなってしまった。
    googleのアカウントをいくつか作って、試せるようにする。

---
+++ YouTubeApp1/StoryBoards/Search.swift
@@ -0,0 +1,8 @@
+//
+//  Search.swift
+//  YouTubeApp1
+//
+//  Created by USER on 2020/07/28.
+//
+
+import Foundation
--- YouTubeApp1/ViewControllers /ViewController.swift
+++ YouTubeApp1/ViewControllers /ViewController.swift
@@ -15,67 +15,131 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var profileImageView: UIImageView!
     
-    private let cellId = "cellId"
-    private var videoItems = [Item]()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        videoListCollectionView.delegate = self
-        videoListCollectionView.dataSource = self
-        
-        videoListCollectionView.register(UINib(nibName: "VideoListCell", bundle: nil), forCellWithReuseIdentifier: cellId)
-        
-        profileImageView.layer.cornerRadius = 20
-        
-        fetchYoutubeSerachInfo()
-    }
-    
-    private func fetchYoutubeSerachInfo() {
-        let params = ["q": "lebronjames"]
-        
-        API.shared.request(path: .search, params: params, type: Video.self) { (video) in
-            self.videoItems = video.items
-            let id = self.videoItems[0].snippet.channelId
-            self.fetchYoutubeChannelInfo(id: id)
-        }
-    }
+    @IBOutlet weak var headerView: UIView!
     
     
-    private func fetchYoutubeChannelInfo(id: String) {
-        let params = [
-            "id": id
-        ]
-        
-        API.shared.request(path: .channels, params: params, type: Channel.self) { (channel) in
-            self.videoItems.forEach { (item) in
-                item.channel = channel
-            }
-            
-            self.videoListCollectionView.reloadData()
-        }
-    }
+    @IBOutlet weak var headerHeightConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var headerTopConstraint: NSLayoutConstraint!
+    
+    
+    
+    private var prevContentOffset: CGPoint = .init(x: 0, y: 0)
+       private let headerMoveHeight: CGFloat = 5
+       
+       private let cellId = "cellId"
+       private var videoItems = [Item]()
+       
+       override func viewDidLoad() {
+           super.viewDidLoad()
+           
+           videoListCollectionView.delegate = self
+           videoListCollectionView.dataSource = self
+           
+           videoListCollectionView.register(UINib(nibName: "VideoListCell", bundle: nil), forCellWithReuseIdentifier: cellId)
+           
+           profileImageView.layer.cornerRadius = 20
+           
+           fetchYoutubeSerachInfo()
+       }
+       
+       private func fetchYoutubeSerachInfo() {
+           let params = ["q": "lebronjames"]
 
-}
+           API.shared.request(path: .search, params: params, type: Video.self) { (video) in
+               self.videoItems = video.items
+               let id = self.videoItems[0].snippet.channelId
+               self.fetchYoutubeChannelInfo(id: id)
+           }
+       }
+       
+       
+       private func fetchYoutubeChannelInfo(id: String) {
+           let params = [
+               "id": id
+           ]
+           
+           API.shared.request(path: .channels, params: params, type: Channel.self) { (channel) in
+               self.videoItems.forEach { (item) in
+                   item.channel = channel
+               }
+               
+               self.videoListCollectionView.reloadData()
+           }
+       }
+       
+       func scrollViewDidScroll(_ scrollView: UIScrollView) {
+           DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+               self.prevContentOffset = scrollView.contentOffset
+           }
+           
+           guard let presentIndexPath = videoListCollectionView.indexPathForItem(at: scrollView.contentOffset) else { return }
+           if scrollView.contentOffset.y < 0 { return }
+           if presentIndexPath.row >= videoItems.count - 2 { return }
+           
+           let alphaRatio = 1 / headerHeightConstraint.constant
+                   
+           if self.prevContentOffset.y < scrollView.contentOffset.y {
+               if headerTopConstraint.constant <= -headerHeightConstraint.constant { return }
+               headerTopConstraint.constant -= headerMoveHeight
+               headerView.alpha -= alphaRatio * headerMoveHeight
+           } else if self.prevContentOffset.y > scrollView.contentOffset.y {
+               if headerTopConstraint.constant >= 0 { return }
+               headerTopConstraint.constant += headerMoveHeight
+               headerView.alpha += alphaRatio * headerMoveHeight
+           }
+           
+           print("self.prevContentOffset: ", self.prevContentOffset, "scrollView.contentOffset: ", scrollView.contentOffset)
+       }
+       
+       func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+           if !decelerate {
+               headerViewEndAnimation()
+           }
+       }
 
-extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = self.view.frame.width
-        
-        return .init(width: width, height: width)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return videoItems.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = videoListCollectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! VideoListCell
-        cell.videoItem = videoItems[indexPath.row]
-        
-        return cell
-    }
+       func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+           headerViewEndAnimation()
+       }
+       
+       private func headerViewEndAnimation() {
+           if headerTopConstraint.constant < -headerHeightConstraint.constant / 2 {
+               UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0.8, options: [], animations: {
+                   
+                   self.headerTopConstraint.constant = -self.headerHeightConstraint.constant
+                   self.headerView.alpha = 0
+                   self.view.layoutIfNeeded()
+               })
+           } else {
+               UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0.8, options: [], animations: {
+                   
+                   self.headerTopConstraint.constant = 0
+                   self.headerView.alpha = 1
+                   self.view.layoutIfNeeded()
+               })
+           }
+       }
+       
+   }
 
-    
-}
+   extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+       
+       func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+           let width = self.view.frame.width
+           
+           return .init(width: width, height: width)
+       }
+       
+       func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+           return videoItems.count
+       }
+       
+       func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+           let cell = videoListCollectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! VideoListCell
+           cell.videoItem = videoItems[indexPath.row]
+           
+           return cell
+       }
+
+       
+   }
